32d1dfbb39cec9272d6573c6096656b0ade6526c
LLiiaamm <66106514+LiamMullen8@users.noreply.github.com>
2021-06-03 03:10:36 -0400

    Add files via upload

---
+++ ASCII-Image-Converter/main.py
@@ -0,0 +1,69 @@
+from PIL import Image, ImageDraw, ImageFont
+import math
+
+#these are the characters that make up the image, can do many combinations for different effects
+
+chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
+#chars = list(".,/\[]{}()OQPHZNRGKBMX")
+
+charArr = list(chars)
+charLen = len(charArr)
+intvl = charLen/256        #index normalizer
+
+#max val of input is 255
+#multiplying by len/256 gives seminormalized value within chars (ratio * length of list)
+def grey2char(idx):
+  return charArr[math.floor(idx*intvl)]
+
+print("Enter Image Name: ")
+inImg = input()
+
+#open image, get size
+image = Image.open("input_images/" + inImg + ".jpg")
+w, h = image.size
+
+#get scale factor,
+#scale image to try to match original
+print("Enter scale delta: (0-13]")
+print("WARNING: Any number above 10 starts to take a while.")
+delta = float(input())
+delta = delta / 10 #make a bit more reasonable
+
+print("--- ASCII-ifying ---")
+#avg ratio of char size
+W = 6
+H = 9
+
+#init output text file
+out = open("out.txt", "w")
+
+#usually need to squash characters bc theyre not square
+image = image.resize((int(w*delta),int(h*delta*(W/H))), Image.NEAREST)
+w, h = image.size
+
+pix = image.load()
+
+#create output image
+outImg = Image.new('RGB', (W*w, H*h), color = (0,0,0))
+ddraw = ImageDraw.Draw(outImg)
+
+#for each pixel,
+#convert to greyscale
+#greyscale char index coorespond
+#write char to img/txt
+#once end of row, need to writre newline it doesnt know
+for i in range(h):
+  for j in range(w):
+    (r,g,b) = pix[j,i]
+    h = int((r+g+b)/3)
+    pix[j,i] = (h,h,h)
+
+    out.write(grey2char(h))
+    ddraw.text((j*W, i*H), grey2char(h), fill = (r, g, b))
+
+  out.write('\n')
+
+print("Save Image Name: ")
+imgName = input()
+outImg.save("output_images/" + imgName + ".jpg")
+print("!--- Conversion Complete ---!")
